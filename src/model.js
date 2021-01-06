17f5fb87fa96b700d17ca7eb5fa778689a696b6f
Matt Carter <m@ttcarter.com>
2021-01-06 14:34:08 +1000

    BUGFIX: Dead code

--- src/model.js
+++ src/model.js
@@ -10,23 +10,6 @@ module.exports = function(mongoosy) {
 		// Create insert / insertOne aliases
 		model.insert = model.insertOne = model.create;
 
-
-		/*
-		// Plugin should be available even after compiling the model
-		model.plugin = (func, options) => {
-			func(model, options);
-			return model;
-		};
-
-
-		// Glue a virtual method which really redirects to the schema
-		model.virtual = (...args) => {
-			model.virtual(...args);
-			return model;
-		};
-		*/
-
-
 		// Debugging enabled? Strap a debugging prefix onto all doc access methods
 		if (debug.enabled || process.env.DEBUG) {
 			['count', 'create', 'deleteMany', 'deleteOne', 'insert', 'insertOne', 'insertMany', 'updateMany', 'updateOne']
