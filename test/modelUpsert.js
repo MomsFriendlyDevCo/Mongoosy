a719cde12fd97b0374a85cc4710f0aaecb4b5b21
Matt Carter <m@ttcarter.com>
2021-07-21 09:03:56 +1000

    test: More test cases for MODEL.upsert()

--- test/modelUpsert.js
+++ test/modelUpsert.js
@@ -16,17 +16,31 @@ describe('MODEL.upsert()', ()=> {
 			expect(newMovie).to.have.property('name', 'Joe Upsert');
 			expect(newMovie).to.have.property('status', 'unverified');
 		})
+		.then(()=> mongoosy.models.users.count({name: 'Joe Upsert'}))
+		.then(count => expect(count).to.be.equal(1))
 	);
 
 	it('should update an existing user if something matches the existing fields', ()=> Promise.resolve()
 		.then(()=> mongoosy.models.users.upsert({
-			name: 'Joe Upsert',
+			name: 'Joe Upsert2',
+			status: 'unverified',
+		}, ['name']))
+		.then(existingMovie => {
+			expect(existingMovie).to.have.property('_id');
+			expect(existingMovie._id).to.be.a('string');
+			expect(existingMovie).to.have.property('name', 'Joe Upsert2');
+			expect(existingMovie).to.have.property('status', 'unverified');
+		})
+		.then(()=> mongoosy.models.users.count({name: 'Joe Upsert2'}))
+		.then(count => expect(count).to.be.equal(1))
+		.then(()=> mongoosy.models.users.upsert({
+			name: 'Joe Upsert2',
 			status: 'active',
 		}, ['name']))
 		.then(existingMovie => {
 			expect(existingMovie).to.have.property('_id');
 			expect(existingMovie._id).to.be.a('string');
-			expect(existingMovie).to.have.property('name', 'Joe Upsert');
+			expect(existingMovie).to.have.property('name', 'Joe Upsert2');
 			expect(existingMovie).to.have.property('status', 'active');
 		})
 	);
