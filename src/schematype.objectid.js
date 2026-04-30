13bcd6e4c3df68e741cf063942f18a4d615d4f29
Mr-Yellow <mr-yellow@mr-yellow.com>
2026-04-30 14:42:54 +1000

    fix: Updated getter to better handle sub-documents with populate()

--- src/schematype.objectid.js
+++ src/schematype.objectid.js
@@ -1,7 +1,8 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 /**
 * Hack to always auto-convert all ObjectIds to strings
 * @see https://github.com/Automattic/mongoose/issues/6996#issuecomment-434063799
+* @see https://github.com/Automattic/mongoose/issues/16250#event-24978162240
 */
-mongoose.Schema.ObjectId.get(v => v != null ? v.toString() : v);
+mongoose.Schema.ObjectId.get(v => (v instanceof mongoose.Types.ObjectId) ? v.toString() : v);
